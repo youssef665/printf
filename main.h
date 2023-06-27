718f5f28e6a6b9d20be01c986310c626991bedbd
youssef665 <yi559668@gmail.com>
2023-06-27 11:45:55 -0700

    i think there is no more errors

--- main.h
+++ main.h
@@ -92,7 +92,7 @@ int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
 int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
-int *get_width(char *s, params_t *params, va_list ap);
+char *get_width(char *s, params_t *params, va_list ap);
 
 
 /* convert_number.c module */
